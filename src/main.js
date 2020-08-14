955810474009d102dc254b9dd2049b20becf9a71
Tarun Gupta <61280743+tarung10@users.noreply.github.com>
2020-08-14 13:25:22 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
-    $('.header').height($(window).height()/5);
-
+    $('.header').height($(window).height()/10);
+    
     if ($(window).width() <= "755"){
         $('.author-follow').before('<br><div class="follow">+ Follow</div>');
         $('.author-follow').css('margin-left', '30px');
         $('.author-follow').hide();
         $('.vl').hide();
         $('span').html('<hr>')
+        flag = false;
     }
+    
+    if ($('.about').height() > "400"){
+        $('.vl').height($('.about').height() + 150);
+    }
+
     $('.follow').bind('click', function(){
         $('.author-follow').slideToggle();
-    })
+    });
 });
